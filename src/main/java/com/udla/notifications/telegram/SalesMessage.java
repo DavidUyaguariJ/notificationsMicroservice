71a069912feacb9607014b9e1619e24695dd32b6
Mateo <mateo.ramirez.lopez@udla.edu.ec>
2025-01-21 01:15:41 -0500

    feat: change Token

--- src/main/java/com/udla/notifications/telegram/SalesMessage.java
+++ src/main/java/com/udla/notifications/telegram/SalesMessage.java
@@ -17,7 +17,7 @@ public class SalesMessage {
 
     @RabbitListener(queues = ConsumerConfiguration.QUEUE_NOTIFICATIONS_SALES)
     public void sendSaleMessage(MessageModel message){
-        
+
         StringBuilder strMessage = new StringBuilder();
         strMessage.append("Hola").append("\n").append("Estimado: ")
         .append(message.getName()).append("Su venta se ha realizado con exito por el producto ")
@@ -28,7 +28,7 @@ public class SalesMessage {
         
             String telegramApiUrl = String.format(
                 "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s",
-                "7750793510:AAH4n95zLjXDaa8AQ1N92YFRY3lCKRrMlOk", message.getIdClient(), strMessage.toString()
+                "Token", message.getIdClient(), strMessage.toString()
             );
             restTemplate.getForObject(telegramApiUrl, String.class);
         } catch (Exception e) {
